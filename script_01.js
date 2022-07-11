5851acfce4fc6d71c27d3731dca7f7e22113d117
BaeckerAnna <baecker.anna2@gmail.com>
2022-07-11 09:16:43 +0200

    If Else

--- script_01.js
+++ script_01.js
@@ -2,3 +2,50 @@
 
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assignment
+const ageJohn = 25;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+let isMarkOlder, isMarkEqual;
+
+//Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************* IF *************/
+// TINA --> There is no alternative!
+// entweder Ja oder nix ... alternativlos
+
+// if (true)
+// if (false)
+// if (isJohnOlder)
+// if (ageJohn > ageMark)
+// {
+//     console.log("John ist älter!");
+// }
+
+/************* IF - ELSE *************/
+// mit Alternative
+// entweder Ja oder nein
+
+
+if (isJohnOlder) 
+{
+    console.log("John ist älter!");
+} 
+else 
+{
+    console.log("John ist jünger!");
+}
+
+/************* Ternäre (ternary Schreibweise) *************/
+// console.log(isJohnOlder) ? "John is older" : "Mark is older";
